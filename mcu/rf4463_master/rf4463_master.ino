d70aa89a782ba9f28db3662e7898e65a4da07627
Kevin Lutzer <kevinlutzer9@gmail.com>
2023-06-28 02:38:19 -0400

    get the slave and master code to work with one another

--- mcu/rf4463_master/rf4463_master.ino
+++ mcu/rf4463_master/rf4463_master.ino
@@ -2,74 +2,50 @@
 #include <SPI.h>
 #include <Wire.h>
 
-#define TX_LED A0 // PD3 -> D14
-#define RX_LED A6 // PD5 -> D20
-#define TRIG 3
+// rf4463_master.pde
 
-RF4463 rf4463;
+// Caution:RF4463 can only work under 3.3V
+// please make sure the supply of you board is under 3.3V
+// 5v supply will destroy RF4463 module!!
+
+// This code runs in master mode and  works with rf4463_slave.pde 
+// Flow:receive "T" from serial->send a packet->wait for reply
+// data of packet is "swwxABCDEFGHIm"
+
+#include <RF4463.h>
+#include <SPI.h>
+
+#define IRQ 2
+#define SDN 9 // 
+#define SEL 10 // 
+
+RF4463 rf4463 = RF4463(IRQ, SDN, SEL);
 unsigned char tx_buf[]={"swwxABCDEFGHIm"};
 unsigned char val;
-
-bool tx_led_status = 0;
-bool rx_led_status = 0;
-
-void changeLEDStatus() {
-  tx_led_status = !tx_led_status;
-  rx_led_status = !rx_led_status;
-  Serial.println("\n rx led: ");
-  Serial.print(rx_led_status, DEC);
-  Serial.println("\n tx led: ");
-  Serial.print(tx_led_status, DEC);
-}
+unsigned char flag=0;    //  flag of rx mode
+unsigned char rx_len;
+unsigned char rx_buf[20];
+uint8_t count = 0;
 
 void setup() {
-  Wire.begin(4);
   Serial.begin(9600);
-
-  Serial.println("Starting connection...");
-
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(TX_LED, OUTPUT);
-  pinMode(RX_LED, OUTPUT);
-
-  digitalWrite(LED_BUILTIN, HIGH);
 
   if(!rf4463.init())
     Serial.println("Init fail!");
-  
-  rf4463.enterStandbyMode();
-  attachInterrupt(digitalPinToInterrupt(TRIG), changeLEDStatus, CHANGE);
+  else
+    Serial.println("Init success!");
+
+  // if(!rf4463.enterStandbyMode())
+  //   Serial.println("Failed to enter standby mode!");
 }
 
-void receiveEvent(int howMany)
+void loop() {
+  rf4463.txPacket(tx_buf,sizeof(tx_buf));
 
-{
-  Serial.println("Recieved Data! Size is: ");
-  Serial.print(howMany, DEC);
+  Serial.println("tx ");
+  Serial.print(count, DEC);
+  Serial.println("\n");
+  count += 1;
 
-  while(1 < Wire.available()) { // loop through all but the last
-    char c = Wire.read(); // receive byte as a character
-    Serial.print(c);         // print the character
-  }
-
-  int x = Wire.read();    // receive byte as an integer
-
-  Serial.println(x);         // print the integer
-  Wire.onRequest(receiveEvent);
-}
-
-void loop() 
-{
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    digitalWrite(TX_LED, tx_led_status);
-    digitalWrite(RX_LED, rx_led_status);
-
-    val=Serial.read();  // please make sure serial is OK befor runing this code
-    if(val=='T') {    // tx a packet if receive "T"
-      rf4463.txPacket(tx_buf,sizeof(tx_buf));
-      Serial.println("tx");
-      rf4463.enterStandbyMode();      // go to sleep till next tx
-    }
+  delay(10);
 }
--- mcu/rf4463_slave/rf4463_slave.ino
+++ mcu/rf4463_slave/rf4463_slave.ino
@@ -1,31 +1,58 @@
-#include<RF4463.h>
-#include <SPI.h>
+// rf4463_slave.pde
 
+// Caution:RF4463 module can only work under 3.3V
+// please make sure the supply of you board is under 3.3V
+// lora supply will destroy RF4463 module!!
+
+// This code runs in slave mode and  works with rf_4463_master.pde 
+// Flow:receive packet from master->print to serial->reply
+// data of packet is "swwxABCDEFGHIm"
+
+#include<RF4463.h>
+#include<SPI.h>
+ 
 RF4463 rf4463;
+unsigned char tx_buf[]={"swwxABCDEFGHIm"};
+unsigned char val;
 unsigned char flag=1;    //  flag of rx mode
 unsigned char rx_len;
 unsigned char rx_buf[20];
 
+#define DEBUG_RF4463
+
 void setup() {
   Serial.begin(9600);
-  if(!rf4463.init())
-  {
-     Serial.println("Init fail!");
+  if(!rf4463.init()) {
+    Serial.println("Init fail!");
+  } else {
+    Serial.println("Init success!");
   }
-   rf4463.rxInit();    // wait for packet from tx
+
+
+  if(!rf4463.checkDevice()) {
+    Serial.println("Check device failed");
+  }
+  rf4463.rxInit();
+
+  uint8_t buf[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  if(rf4463.getCommand(7,RF4463_CMD_START_RX,buf)) {
+    Serial.println("Got the CMD to setup rx");
+  }
+
+  for(uint8_t i =  0; i < sizeof(buf); i = i + 1) {
+    Serial.println("Write byte: ");
+    Serial.write(i);
+    Serial.write(buf[i]);
+  }
+
 }
 void loop() 
 {
-    if(flag==1)
-    {
-        if(rf4463.waitnIRQ())    // wait for PACKET_RX interrupt
-        {
-            rf4463.clrInterrupts();
-            rx_len=rf4463.rxPacket(rx_buf);  // read rx data
-            Serial.write(rx_buf,rx_len);    // print out by serial
-            Serial.println();
-            rf4463.rxInit();    // wait for packet from tx
-        }
-    }
-
+  if(rf4463.waitnIRQ()) {
+    rf4463.clrInterrupts();
+    rx_len=rf4463.rxPacket(rx_buf);  // read rx data
+    Serial.write(rx_buf, rx_len);    // print out by serial
+    Serial.println();
+    rf4463.rxInit();
+  }
 }
