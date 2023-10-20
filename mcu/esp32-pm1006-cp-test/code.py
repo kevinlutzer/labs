c0f6c2d84c30dffec7332b6dee29d0bf6caa2a08
Kevin Lutzer <kevinlutzer@proton.me>
2023-10-20 17:23:23 -0400

    update for i2c scan, add anolog read for a photodiode

--- mcu/esp32-pm1006-cp-test/code.py
+++ mcu/esp32-pm1006-cp-test/code.py
@@ -2,17 +2,49 @@ import board
 import digitalio
 import busio
 import time
+from analogio import AnalogIn
+
+
+uart = busio.UART(tx=board.IO5, rx=board.IO4, baudrate=9600)
+i2c = busio.I2C(board.IO6, board.IO7)
+pd_in = AnalogIn(board.IO3)
 
 FAN = board.IO10
 
-print(FAN)
-pin = digitalio.DigitalInOut(FAN)
-pin.direction = digitalio.Direction.OUTPUT
+def pm1006_ping():
 
-pin.value = True
+    print(FAN)
+    pin = digitalio.DigitalInOut(FAN)
+    pin.direction = digitalio.Direction.OUTPUT
 
-uart = busio.UART(tx=board.IO5, rx=board.IO4, baudrate=9600)
-while True:
-    time.sleep(1)
-    print(uart.write(bytearray(b'.\x11\x02\x0b\x01\xe1')))
-    print(uart.read(8))
+    pin.value = True
+
+    while True:
+        time.sleep(1)
+        print(uart.write(bytearray(b'.\x11\x02\x0b\x01\xe1')))
+        print(uart.read(16))
+
+def i2c_scanner():
+
+    while not i2c.try_lock():
+        pass
+
+    try:
+        while True:
+            print(
+                "I2C addresses found:",
+                [hex(device_address) for device_address in i2c.scan()],
+            )
+            time.sleep(2)
+
+    finally:  # unlock the i2c bus when ctrl-c'ing out of the loop
+        i2c.unlock()
+
+def read_pd():
+    pd_in
+    while True:
+        val = (pd_in.value * 3.3) / 65536
+        print("Val: ", val)
+        time.sleep(1)
+
+pm1006_ping()
